ca09121128b0139e06bcf2a1ba984626e49e340b
rehanmalek786 <rehanmalek259@gmail.com>
2026-08-09 18:52:09 +0530

    Remove Java version configuration for Android plugins
    Removed Java compile options configuration for Android plugins.

--- overrides/root_append.gradle.kts
+++ overrides/root_append.gradle.kts
@@ -9,14 +9,6 @@ buildscript {
 }
 
 subprojects {
-    plugins.withType<com.android.build.gradle.BasePlugin> {
-        extensions.configure<com.android.build.gradle.BaseExtension> {
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-        }
-    }
     plugins.withType<org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper> {
         extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension> {
             jvmToolchain(17)
