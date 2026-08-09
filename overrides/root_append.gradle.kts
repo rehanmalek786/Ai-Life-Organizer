84ba1a57f7234b1a13073d31c0589d9dfc15e6af
rehanmalek786 <rehanmalek259@gmail.com>
2026-08-09 18:20:09 +0530

    Configure Firebase and set Java/Kotlin compatibility
    Added Firebase dependencies and configured Java/Kotlin compatibility.

--- overrides/root_append.gradle.kts
+++ overrides/root_append.gradle.kts
@@ -1,4 +1,3 @@
-
 // ---- Added for Firebase (AI Life Organizer) ----
 buildscript {
     repositories {
@@ -7,5 +6,21 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
+subprojects {
+    afterEvaluate {
+        extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+        }
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+            }
+        }
     }
 }
