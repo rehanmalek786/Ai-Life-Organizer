be190be27f8181de241de88b8378b27b0deb5361
rehanmalek786 <rehanmalek259@gmail.com>
2026-08-09 19:12:54 +0530

    Add core library desugaring support
    Enable core library desugaring and add dependency.

--- overrides/app_append.gradle.kts
+++ overrides/app_append.gradle.kts
@@ -2,4 +2,11 @@ android {
     defaultConfig {
         minSdk = 23
     }
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
