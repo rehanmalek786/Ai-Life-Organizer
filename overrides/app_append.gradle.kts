11afe4d66169b33c17035c236fd8715190f3c284
rehanmalek786 <rehanmalek259@gmail.com>
2026-08-09 18:44:31 +0530

    Upgrade Java and Kotlin compatibility to version 17

--- overrides/app_append.gradle.kts
+++ overrides/app_append.gradle.kts
@@ -3,13 +3,7 @@ android {
         minSdk = 23
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
