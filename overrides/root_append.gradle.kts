aca33415e4b20b24e485027f42fa7577ac3c22fb
rehanmalek786 <rehanmalek259@gmail.com>
2026-08-09 19:00:58 +0530

    Remove Kotlin jvmToolchain configuration from Gradle

--- overrides/root_append.gradle.kts
+++ overrides/root_append.gradle.kts
@@ -7,11 +7,3 @@ buildscript {
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
-
-subprojects {
-    plugins.withType<org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper> {
-        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension> {
-            jvmToolchain(17)
-        }
-    }
-}
