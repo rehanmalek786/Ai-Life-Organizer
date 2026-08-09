48ea3bc9ec3762399a3b114163a1e83edd82f0ee
rehanmalek786 <rehanmalek259@gmail.com>
2026-08-09 18:51:37 +0530

    Update Kotlin JVM toolchain and remove compile options

--- overrides/app_append.gradle.kts
+++ overrides/app_append.gradle.kts
@@ -2,8 +2,8 @@ android {
     defaultConfig {
         minSdk = 23
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
