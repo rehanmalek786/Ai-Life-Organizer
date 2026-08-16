35f191129afc1cfa60b74a859b80c03bdaeea291
rehanmalek786 <rehanmalek259@gmail.com>
2026-08-16 15:17:14 +0530

    Update settings_screen.dart

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../../providers/app_providers.dart';
 import '../../services/firestore_service.dart';
